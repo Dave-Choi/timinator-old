7415a57058b897f7bd48dcc4652f709bfb6adf47
Rails Dev <railsdev@MacBookProBot.local>
2013-07-17 04:35:37 -0400

    Cleanup whitespace

--- app/assets/javascripts/util/solve_result.js
+++ app/assets/javascripts/util/solve_result.js
@@ -2,17 +2,17 @@ Timinator.SolveResult = Ember.Object.extend({
   method: null,
   times: null,
   scramble: "",
-  
+
   init: function(){
     this.set("times", []);
-    
+
     this.set("scramble", this.get("scramble") || Timinator.ScrambleGenerator.generate3x3x3());
   },
-  
+
   pushTime: function(time){
     this.get("times").pushObject(time);
   },
-  
+
   total: function(){
     var times = this.get("times");
     var totalTime = 0;
