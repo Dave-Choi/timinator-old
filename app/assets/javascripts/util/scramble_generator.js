19e2f2d384fa806c449bb859fd00788a44a59738
Rails Dev <railsdev@MacBookProBot.local>
2013-07-24 17:31:32 -0400

    Fix redundant turn generation in ScrambleGenerator.generate3x3x3()
    The old version allowed for 3 coaxial turns in a row, which is
    redunant.
    
    New version still has fundamental flaws of 25 random turns, rather than
    generating a random cube state and working backward.

--- app/assets/javascripts/util/scramble_generator.js
+++ app/assets/javascripts/util/scramble_generator.js
@@ -4,20 +4,42 @@ Timinator.ScrambleGenerator = Ember.Object.create({
 	},
 
 	generate3x3x3: function(){
-		var faces = ["U", "D", "L", "R", "F", "B"];
+		// This is too-much-coffee-fueled cutesy crap, 
+		var faces = ["U", "L", "F", "D", "R", "B"]; // These are ordered so that you can identify the axis by: index % 3
 		var directions = ["", "'", "2"];
-		var lastFace = "", nextFace = "";
-		var buff = "";
 
-		var i, numTurns = 25;
+		var turns = [];
+		var i, numTurns = 25, numFaces = faces.length;
+		var lastIndex = Math.floor(Math.random() * numFaces);
+		var randIndex;
+
 		for(i=0; i<numTurns; i++){
-			while(lastFace == nextFace){
-				nextFace = this.randomElement(faces);
-			}
-			lastFace = nextFace;
-			buff += nextFace + this.randomElement(directions) + " ";
+			/*
+				The approach here is to add a number, N, to the last index, 
+				then modulo by the number of faces to wrap around, 
+				such that you can't repeat a face.
+
+				N is at least 1, and at most 5 (numFaces - 1).
+			*/
+			do{
+				var N = Math.floor(Math.random() * (numFaces - 1)) + 1;
+				randIndex = (lastIndex + N) % numFaces;
+			} while(
+				// We have to check for redundant sequences. i.e. 3 turns in a row can't be coaxial.
+				((lastIndex % 3) == (randIndex % 3)) && // We're okay if this turn and the last one aren't coaxial.
+				turns[i-2] && // We're okay if we don't have 3 turns yet. (array out of bounds is 'undefined' in JS, and this makes the .faceIndex lookup in the next clause safe)
+				((turns[i-2].faceIndex % 3) == (randIndex % 3 )) // We're okay if this turn and the one two turns ago aren't coaxial
+			);
+
+			lastIndex = randIndex;
+			turns.push({
+				faceIndex: randIndex,
+				direction: this.randomElement(directions)
+			});
 		}
 
-		return buff;
+		return turns.map(function(turn){
+			return faces[turn.faceIndex] + turn.direction;
+		}).join(" ");
 	}
 });
