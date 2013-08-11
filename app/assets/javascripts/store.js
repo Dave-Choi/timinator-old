7c0f1a374714ab6783f4e09278abeaa44885983a
Rails Dev <railsdev@MacBookProBot.local>
2013-08-11 05:56:32 -0400

    Set up DS.FixtureAdapter for debugging.

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,3 +1,22 @@
 Timinator.Store = DS.Store.extend({
-	adapter: DS.RESTAdapter.create()
+	adapter: DS.FixtureAdapter.extend({
+        queryFixtures: function(fixtures, query, type) {
+            console.log(query);
+            console.log(type);
+            console.log(fixtures);
+
+            var records = fixtures.filter(function(item) {
+                for(var prop in query) {
+                    if( item[prop] != query[prop]) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+
+            console.log(records);
+
+            return records;
+        }
+    })
 });
