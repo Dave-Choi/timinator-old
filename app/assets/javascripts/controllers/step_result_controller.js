ab0ee9781eef69ed6fea4ead8f0aa54ca59aff77
Rails Dev <railsdev@MacBookProBot.local>
2013-08-19 03:52:31 -0400

    Overhaul TimerController and template
    This is still unfinished, but I need to get it into source control,
    because I'm scared.

---
+++ app/assets/javascripts/controllers/step_result_controller.js
@@ -0,0 +1,17 @@
+Timinator.StepResultController = Ember.ObjectController.extend({
+	needs: ["solves"],
+	// solvesBinding: "controllers.solves",
+	// solveBinding: "controllers.solve",
+
+	differenceFromAverage: function(){
+		var stepAverage = this.get("controllers.solves").stepAverage(this.get("step"));
+
+		return this.get("time") - stepAverage;
+	}.property("time", "controllers.solves.stepStats"),
+
+	percentOfTotal: function(){
+		var total = this.get("solve.totalTime");
+
+		return (this.get("time") / total) || 0;
+	}.property("time", "solve.totalTime")
+});
