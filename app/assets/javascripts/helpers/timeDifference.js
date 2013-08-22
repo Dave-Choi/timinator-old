f86eb3d0ff2d11fca2d34ba277d41e35d9be41b0
Rails Dev <railsdev@MacBookProBot.local>
2013-08-22 07:33:29 -0400

    Add comment to explain the purpose of the timeDifference helper

--- app/assets/javascripts/helpers/timeDifference.js
+++ app/assets/javascripts/helpers/timeDifference.js
@@ -1,3 +1,11 @@
+/*
+	Used to display the difference between a time and the average
+
+	Uses the Bootstrap classes "text-success" and "text-error" to
+	show red and green times, depending on whether the time was
+	worse or better than average, respectively.
+*/
+
 Ember.Handlebars.helper("timeDifference", function(value){
 	var className = "text-success";
 	value = Timinator.Math.thousandthPrecision(value);
