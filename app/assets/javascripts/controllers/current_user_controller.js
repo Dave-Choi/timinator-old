9f1328d924e42066ff9b85a56d7007e7c31351af
Rails Dev <railsdev@MacBookProBot.local>
2013-09-11 18:46:57 -0400

    Replace console logs in CurrentUserController with alerts

--- app/assets/javascripts/controllers/current_user_controller.js
+++ app/assets/javascripts/controllers/current_user_controller.js
@@ -1,4 +1,6 @@
 Timinator.CurrentUserController = Ember.ObjectController.extend({
+    needs: ["alerts"],
+
     isLoggedIn: function(){
         return this.get("model") != null;
     }.property("model"),
@@ -11,43 +13,50 @@ Timinator.CurrentUserController = Ember.ObjectController.extend({
     login: function(data){
         var url = "users/sign_in.json";
         var controller = this;
+        var alerts = this.get("controllers.alerts");
         $.post(url, data).then(function(response){
             controller.loadUser(response.user);
-            console.log("log in success");
+            alerts.addAlert("You've successfully logged in.");
         }, function(response){
-            console.log("log in failure");
+            alerts.addAlert("Something went wrong trying to log you in.");
         });
     },
 
     logout: function(){
         var url = "users/sign_out.json";
         var controller = this;
+        var alerts = this.get("controllers.alerts");
         $.ajax({
             url: url,
             type: "delete"
         }).then(function(response){
-            console.log("logged out");
-            console.log(response);
+            alerts.addAlert("You've successfully logged out.");
             controller.set("model", null);
         }, function(){
-            console.log("logout failed");
+            alerts.addAlert("Something went wrong trying to log you out.");
         });
     },
 
     register: function(data){
         var url = "users.json";
         var controller = this;
+        var alerts = this.get("controllers.alerts");
         $.ajax({
             url: url,
             data: data,
             type: "post"
         }).then(function(response){
-            console.log("sign up success");
-            console.log(response);
-            controller.loadUser(response.user);
+            var user = response.user;
+            alerts.addAlert("You've signed up successfully as " + user.email);
+            controller.loadUser(user);
         }, function(response){
-            console.log("sign up failed");
-            console.log(response);
+            var errorList = "";
+            var errors = response.responseJSON.errors;
+            for(var key in errors){
+                var value = errors[key];
+                errorList += key + ": " + value.join(", ");
+            }
+            alerts.addAlert("There was a problem with your sign up: " + errorList);
         })
     }
 });
