f2ab384f89525f3711dfa925fe19d2795168515d
Rails Dev <railsdev@MacBookProBot.local>
2013-10-01 16:13:21 -0400

    Add schema.rb change from commit aae8c0998941f56bbde2948d6a03e0d764dcf1c2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130921060422) do
+ActiveRecord::Schema.define(version: 20130926073410) do
 
   create_table "puzzles", force: true do |t|
     t.string   "name"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20130921060422) do
   end
 
   create_table "solves", force: true do |t|
-    t.datetime "datetime"
     t.string   "scramble"
     t.datetime "created_at"
     t.datetime "updated_at"
