af8908d52d716306dc96d5b45ae886458f1384e7
Rails Dev <railsdev@MacBookProBot.local>
2013-10-01 16:20:11 -0400

    Add resourceful routes for Rails models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Timinator::Application.routes.draw do
   devise_for :users, controllers: { sessions: "sessions" }
   root :to => 'pages#home'
+
+  resources :puzzles, :steps, :solves, :solve_methods, :step_results, only: [:index, :show, :create, :destroy, :update]
 end
